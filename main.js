9e9d30d186dfa5ade262407efeae641288f75c48
Francesco <fra.iozzia@gmail.com>
2020-04-22 18:04:59 +0200

    stars

--- main.js
+++ main.js
@@ -33,13 +33,34 @@ $(document).ready(function(){
 
                   var listaFinale= risultato [i];
 
+                  // milestone 2
+
+
+
+                  var votoHtml = Math.ceil((listaFinale.vote_average)/2);
+
+
+                    var votoStelle = "";
+                    for(var i = 0; i < votoHtml; i++){
+                        votoStelle += '<i class="fas fa-star"></i>';
+                    }
+                    for(var j = 0; j < (5 - votoHtml); j++){
+                        votoStelle += '<i class="far fa-star"></i>';
+                    }
+
+
+
+
+
                   //
                   var context={
                     title: listaFinale.title,
                     subtitle: listaFinale.original_title,
                     language: listaFinale.original_language,
-                    voto: listaFinale.vote_count
+                    voto : votoStelle
                   };
+
+
                   // ...
                   var risultatoDaAggiungere = template(context);
                   $(".container").append(risultatoDaAggiungere);
@@ -48,10 +69,70 @@ $(document).ready(function(){
               },
               error: function(richiesta, stato, errori){
               }
-              //  fine chiamata
+            });
+              // fine chiamata ajax....
+
+              // chiamata serie tv
+  $.ajax({
+            url:"https://api.themoviedb.org/3/search/tv",
+            method: "GET",
+            dataType: "json",
+            data: {
+                api_key: "aecc5cd7bf4a2c1736a6c6b92e0a3b8b",
+                language: "it-IT",
+                query: valoreRicerca,
+            },
+            success: function(data){
+              // handlebars
+              var source = $("#entry-template").html();
+              var template = Handlebars.compile(source);
+              console.log(data);
+              var risultatoSerie= data.results;
+              console.log(risultatoSerie)
+              // ciclo risultati
+              for(var i = 0; i < risultatoSerie.length; i++){
+                var elencoSerie= risultatoSerie[i];
+                //  milestone 2 stelle
+
+                var votoSerieHtml = Math.ceil((elencoSerie.vote_average)/2);
+                var votoStelleSerie = "";
+                for(var i = 0; i < votoSerieHtml; i++){
+                    votoStelleSerie += '<i class="fas fa-star"></i>';
+                }
+                for(var j = 0; j < (5 - votoSerieHtml); j++){
+                    votoStelleSerie += '<i class="far fa-star"></i>';
+                }
 
 
-              })
+
+                var contextTv={
+                  title: elencoSerie.original_name,
+                  subtitle: elencoSerie.name,
+                  language: elencoSerie.original_language,
+                  voto: votoStelleSerie
+                };
+
+                var finalResult= template(contextTv);
+                $(".container").append(finalResult);
+
+              }
+            },
+            error: function(richiesta, stato, errori){
+            }
+            //
+
+
+
+
+          });
+
+
+
+
+
+
+
+
 
 
 
